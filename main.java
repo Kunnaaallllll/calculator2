9300f9eca61fcd2c1109f4620d39e753c3ad0141
Kunnaaallllll <869796kunalchaudhary@gmail.com>
2025-01-17 21:28:12 +0530

    subtract here second commit

--- main.java
+++ main.java
@@ -7,11 +7,23 @@ public class main {
         return num1 + num2;
     }
 
-    // Main method to test the add function
+    // Function to subtract two numbers
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    // Main method to test the functions
     public static void main(String[] args) {
         int number1 = 5;
         int number2 = 10;
-        int result = add(number1, number2);
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + result);
+
+        // Testing add function
+        int additionResult = add(number1, number2);
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + additionResult);
+
+        // Testing subtract function
+        int subtractionResult = subtract(number1, number2);
+        System.out.println("The difference between " + number1 + " and " + number2 + " is: " + subtractionResult);
     }
 }
+
